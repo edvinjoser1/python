e4787b8fcb4dfe4623ebe43a7f742ecfbcc824cb
edvinjoser1 <edvin.joser@elev.ga.ntig.se>
2019-09-04 13:23:38 +0200

    09/04

---
+++ lek-string.py
@@ -0,0 +1,30 @@
+firstName = "Edvin"
+gamerTag = "Teck"
+lastName = "josér"
+
+print(lastName.capitalize())
+
+# för.efternamn
+#firstName[3].lastName[3]
+
+
+userName = firstName[0:3].lower()
+userName += lastName[0:3].lower()
+userName += "19"
+print(userName)
+
+
+
+
+
+tecken = """parenteser()
+[] hakparenters
+{} måsvingar; krullparenteser
+: kolon
+; semikolon
+, komma  
+\" dubbelfnutt, 
+\' enkelfnutt"""
+
+print(tecken[13:26])
+
--- lek1.py
+++
@@ -1,7 +0,0 @@
-firstName = "Tejo"
-lastName = 'JUNEVALL'
-dateOfBirth = 200103295077
-age = 12
-teacher = False
-print(firstName , lastName + str(age) + str(teacher))
-print(dateOfBirth + " ")
